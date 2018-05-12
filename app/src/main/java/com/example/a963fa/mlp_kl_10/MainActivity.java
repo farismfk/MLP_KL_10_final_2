67a17ed4542390307a36d614dfad58c65a30da1d
farismfk <963farisk@gmail.com>
2018-05-12 17:23:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a963fa/mlp_kl_10/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a963fa.mlp_kl_10;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+}
